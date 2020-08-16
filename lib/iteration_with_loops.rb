86d7e55d2a4d0b4cdf7ff75cb32cd4c793ef97f3
James P Bondeson <jpbond58@gmail.com>
2020-08-16 18:02:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
 
         if src[row_index][element_index]
-          src.flatten.select {|item| item.is_a? string}.join ""
+          src.flatten.select {|item| item.is_a? string}.join string_elements
           string_result << src[row_index][element_index]
 
         element_index
